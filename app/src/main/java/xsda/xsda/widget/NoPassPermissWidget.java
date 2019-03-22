26327662da23090fd219d4abc7262dbed1652094
qianli.ma <qianli.ma@jrdcom.com>
2019-03-22 19:28:12 +0800

    继续完善权限未全部通过的逻辑2

--- app/src/main/java/xsda/xsda/widget/NoPassPermissWidget.java
+++ app/src/main/java/xsda/xsda/widget/NoPassPermissWidget.java
@@ -2,8 +2,12 @@ package xsda.xsda.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.hiber.tools.layout.PercentRelativeLayout;
+
+import xsda.xsda.R;
 
 /*
  * Created by qianli.ma on 2019/3/22 0022.
@@ -20,5 +24,9 @@ public class NoPassPermissWidget extends PercentRelativeLayout {
 
     public NoPassPermissWidget(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        View inflate = inflate(context, R.layout.widget_no_pass_permiss, this);
+        ImageView ivBg = inflate.findViewById(R.id.iv_bg_no_pass);
+        View viewById = inflate.findViewById(R.id.tv_no_pass_cancel);
+        View viewById = inflate.findViewById(R.id.tv_no_pass_ok);
     }
 }
