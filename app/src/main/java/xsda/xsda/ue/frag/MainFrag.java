134b3fa009dbdcc659a5c1d8b61a91e85907b263
qianli.ma <qianli.ma@jrdcom.com>
2019-04-03 13:10:08 +0800

    继续开发main页

--- app/src/main/java/xsda/xsda/ue/frag/MainFrag.java
+++ app/src/main/java/xsda/xsda/ue/frag/MainFrag.java
@@ -1,8 +1,7 @@
 package xsda.xsda.ue.frag;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.avos.avoscloud.AVObject;
 
@@ -10,16 +9,8 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import butterknife.BindView;
-import butterknife.OnClick;
-import cn.sharesdk.framework.Platform;
-import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.wechat.friends.Wechat;
 import xsda.xsda.R;
-import xsda.xsda.bean.LoginBean;
-import xsda.xsda.helper.LoginOrOutHelper;
 import xsda.xsda.utils.Lgg;
-import xsda.xsda.utils.Ogg;
-import xsda.xsda.utils.Tgg;
 import xsda.xsda.widget.OfflineWidget;
 import xsda.xsda.wxapi.WechatInfo;
 
@@ -29,12 +20,13 @@ import xsda.xsda.wxapi.WechatInfo;
 
 public class MainFrag extends BaseFrag {
 
-    @BindView(R.id.widget_offline)
-    OfflineWidget widgetOffline;
-    @BindView(R.id.tv_test_main)
-    TextView tvTestMain;
-    @BindView(R.id.bt_quit_wechat)
-    Button btQuitWechat;
+
+    @BindView(R.id.rcv_main_left)
+    RecyclerView rcvMainLeft;
+    @BindView(R.id.rcv_main_right)
+    RecyclerView rcvMainRight;
+    @BindView(R.id.wd_main_offline)
+    OfflineWidget wdOffline;
 
     @Override
     public int onInflateLayout() {
@@ -67,35 +59,35 @@ public class MainFrag extends BaseFrag {
 
     @Override
     public void otherDevicesLogin(AVObject avo) {
-        widgetOffline.setVisibility(View.VISIBLE);
-        widgetOffline.setOnClickOfflineOkListener(() -> toFrag(getClass(), LoginFrag.class, null, false));
+        wdOffline.setVisibility(View.VISIBLE);
+        wdOffline.setOnClickOfflineOkListener(() -> toFrag(getClass(), LoginFrag.class, null, false));
     }
 
     private void clickEvent() {
-        tvTestMain.setOnClickListener(v -> {
-            LoginOrOutHelper loginOrOutHelper = new LoginOrOutHelper(activity);
-            loginOrOutHelper.setOnLogOutSuccessListener(() -> {
-                LoginBean loginBean = Ogg.readLoginJson(activity);
-                Ogg.saveLoginJson(activity, loginBean.getPhoneNum(), loginBean.getPassword(), false);
-                toFrag(getClass(), LoginFrag.class, null, false);
-                Tgg.show(activity, "登出成功", 2500);
-            });
-            loginOrOutHelper.setOnLogOutFailedListener(e -> Tgg.show(activity, "登出失败", 2500));
-            loginOrOutHelper.logout();
-        });
+        // tvTestMain.setOnClickListener(v -> {
+        //     LoginOrOutHelper loginOrOutHelper = new LoginOrOutHelper(activity);
+        //     loginOrOutHelper.setOnLogOutSuccessListener(() -> {
+        //         LoginBean loginBean = Ogg.readLoginJson(activity);
+        //         Ogg.saveLoginJson(activity, loginBean.getPhoneNum(), loginBean.getPassword(), false);
+        //         toFrag(getClass(), LoginFrag.class, null, false);
+        //         Tgg.show(activity, "登出成功", 2500);
+        //     });
+        //     loginOrOutHelper.setOnLogOutFailedListener(e -> Tgg.show(activity, "登出失败", 2500));
+        //     loginOrOutHelper.logout();
+        // });
     }
 
     @Override
     public boolean onBackPresss() {
-        if (widgetOffline.getVisibility() == View.VISIBLE) {
+        if (wdOffline.getVisibility() == View.VISIBLE) {
             return true;
         }
         return false;
     }
 
-    @OnClick(R.id.bt_quit_wechat)
-    public void onViewClicked() {
-        Platform plat = ShareSDK.getPlatform(Wechat.NAME);
-        plat.removeAccount(true);
-    }
+    // @OnClick(R.id.bt_quit_wechat)
+    // public void onViewClicked() {
+    //     Platform plat = ShareSDK.getPlatform(Wechat.NAME);
+    //     plat.removeAccount(true);
+    // }
 }
